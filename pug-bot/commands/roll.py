60f07f71ef08de2501c4adbd58a01b079b8cff80
stevenktruong <steventruong@g.ucla.edu>
2020-04-16 14:34:15 -0700

    Fixed for real this time

--- pug-bot/commands/roll.py
+++ pug-bot/commands/roll.py
@@ -5,5 +5,5 @@ from random import randint
 
 @check(invalid_range, have_no_pug)
 async def roll(message, pugs, user_input, client):
-    roll_range = user_input["arguments"].split()    
-    await message.channel.send(ROLLED(randint(roll_range[0], roll_range[1])))
+    arguments = user_input["arguments"].split()
+    await message.channel.send(ROLLED(randint(int(roll_range[0]), int(roll_range[1]))))
