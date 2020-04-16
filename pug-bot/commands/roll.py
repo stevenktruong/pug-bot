886f486f67d0493ef7e5d0c43f15e30fe4ee163c
stevenktruong <steventruong@g.ucla.edu>
2020-04-16 14:34:44 -0700

    Fixed a typo

--- pug-bot/commands/roll.py
+++ pug-bot/commands/roll.py
@@ -5,5 +5,5 @@ from random import randint
 
 @check(invalid_range, have_no_pug)
 async def roll(message, pugs, user_input, client):
-    arguments = user_input["arguments"].split()
+    roll_range = user_input["arguments"].split()
     await message.channel.send(ROLLED(randint(int(roll_range[0]), int(roll_range[1]))))
